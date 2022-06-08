80f345023641d78522d51d661490ac45aeddade0
admin <promitay@gmail.com>
2022-06-08 20:44:10 +0300

    Домашняя работа. Урок 1.9 Объекты и классы

--- src/pro/sky/java/course1/lesson1_9/App.java
+++ src/pro/sky/java/course1/lesson1_9/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) {
         Author king = new Author("Стивен", "Кинг");
         Author tolstoy = new Author("Лев", "Толсой");
-        Book deadZone = new Book("Мертвая зона", king,1978);
+        Book deadZone = new Book("Мертвая зона", king, 1978);
         Book warAndPeace = new Book("Война и мир", tolstoy, 1868);
         System.out.println(deadZone.getName() + ", " + deadZone.getYearPublishing() + ", " + deadZone.getAuthorName(deadZone));
         System.out.println(warAndPeace.getName() + ", " + warAndPeace.getYearPublishing() + ", " + warAndPeace.getAuthorName(warAndPeace));
